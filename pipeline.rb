31b6ec181df78e90a03a92f8668eafbe78e74316
Brian Palmer <brian@codekitchen.net>
2019-11-24 09:28:53 -0700

    v1.1

--- pipeline.rb
+++ pipeline.rb
@@ -1,8 +1,8 @@
 class Pipeline < Formula
   desc "Interactive prompt for building up a pipeline of shell commands"
   homepage "https://github.com/codekitchen/pipeline"
-  url "https://github.com/codekitchen/pipeline/releases/download/v1.0/pipeline-1.0.tar.gz"
-  sha256 "83aae11654f73352491a900a9256bcbbec355b382e735d09f47df099afd533b9"
+  url "https://github.com/codekitchen/pipeline/releases/download/v1.1/pipeline-1.1.tar.gz"
+  sha256 "010ccdcbc523eb320b628ef75e77b37a02ad1a1e6fb4f51d4fa8f3d8df70e446"
   head do
     url "https://github.com/codekitchen/pipeline.git"
 
